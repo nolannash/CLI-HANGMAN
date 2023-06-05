3168b68ad66fd760d78912c7059fdd31b385e8ea
nolan nash <n0lan.n4sh@gmail.com>
2023-06-05 10:50:37 -0700

    start player class, realized need to make branch

---
+++ lib/classes/player.py
@@ -0,0 +1,21 @@
+class Player:
+    def __init__(self,name):
+        self.name = name
+
+#instance properties/attributes
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self,name):
+        if isinstance(name,str) and 0<len(name)<=20:
+            self._name = name
+        else:
+            raise AttributeError('Your name must be a String of 20 characters or less')
+
+#instance methods
+
+#class methods
+    @classmethod
+    def create_table(cls):
+        ...
